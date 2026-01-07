2c7cc3fd8e5413ac309b4e922f839072787dba41
ChoiJiHye950 <principle950@daum.net>
2026-01-07 16:51:40 +0900

    feat: BaseEntity, BaseUpdatable 엔티티 추가

---
+++ src/main/java/com/codeit/monew/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.codeit.monew.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(updatable = false, nullable = false)
+  private UUID id;
+
+  @CreatedDate
+  @Column(updatable = false, nullable = false)
+  private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/codeit/monew/domain/BaseUpdatableEntity.java
@@ -0,0 +1,21 @@
+package com.codeit.monew.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseUpdatableEntity extends BaseEntity {
+
+  @LastModifiedDate
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
+
+  protected void update() {
+    this.updatedAt = LocalDateTime.now();
+  }
+}
