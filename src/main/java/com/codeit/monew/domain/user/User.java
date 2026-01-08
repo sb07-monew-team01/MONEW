db2cd858a858116878100fd70623ca8436af50d1
hyo37009 <hyo37009@gmail.com>
2026-01-08 09:49:37 +0900

    feat: User 클래스에 필드 및 생성자 추가 (green)
    - email, nickname, password 필드 추가
    - 해당 필드를 초기화하는 생성자 추가

--- src/main/java/com/codeit/monew/domain/user/User.java
+++ src/main/java/com/codeit/monew/domain/user/User.java
@@ -1,4 +1,13 @@
 package com.codeit.monew.domain.user;
 
 public class User {
+    private String email;
+    private String nickname;
+    private String password;
+
+    public User(String email, String nickname, String password) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
 }
