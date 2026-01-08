8cf194686861eae3ce46f0369ad5863701707699
hyo37009 <hyo37009@gmail.com>
2026-01-08 18:30:07 +0900

    remove: 불필요한 Scanner 필드 삭제
    MonewException 클래스에서 사용되지 않는 Scanner 필드를 제거했습니다.

--- src/main/java/com/codeit/monew/global/exception/MonewException.java
+++ src/main/java/com/codeit/monew/global/exception/MonewException.java
@@ -5,13 +5,11 @@ import com.codeit.monew.global.enums.ErrorCode;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class MonewException extends RuntimeException {
     private final Instant timestamp = Instant.now();
     private final ErrorCode errorCode;
     protected final Map<String, Object> details;
-    Scanner sc = new Scanner(System.in);
 
     public MonewException(Class<?> classType, ErrorCode errorCode, Map<String, Object> details) {
         this.errorCode = errorCode;
