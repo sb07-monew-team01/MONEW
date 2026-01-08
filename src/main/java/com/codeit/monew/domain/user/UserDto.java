ea4c657a9f4bbbc66c5d8d0b4bafa88fc3c4e55e
hyo37009 <hyo37009@gmail.com>
2026-01-08 16:20:14 +0900

    feat: User 회원 가입 기능 관련 클래스 및 테스트 추가 (GREEN)
    - UserDto, UserMapper, UserSignInRequest 클래스 추가
    - UserRepository 인터페이스 추가로 데이터 저장소 매핑 처리
    - UserServiceTest에 회원 가입 테스트 메서드 추가

---
+++ src/main/java/com/codeit/monew/domain/user/UserDto.java
@@ -0,0 +1,13 @@
+package com.codeit.monew.domain.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record UserDto(
+        UUID id,
+        String email,
+        String nickname,
+        LocalDateTime createdAt
+)
+{
+}
