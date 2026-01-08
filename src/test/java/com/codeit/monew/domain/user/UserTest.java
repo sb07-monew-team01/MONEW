34a14e4eca315f5ab037e0ce98494eccd777db52
hyo37009 <hyo37009@gmail.com>
2026-01-08 10:00:32 +0900

    feat: User 닉네임 수정 테스트 추가 (RED)
    - updateNickname 메서드를 검증하는 테스트를 작성
    - 닉네임이 정상적으로 변경되는지 확인

--- src/test/java/com/codeit/monew/domain/user/UserTest.java
+++ src/test/java/com/codeit/monew/domain/user/UserTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
 
     @Test
-    @DisplayName("사용자는 이메일, 닉네임, 비밀번호의 정보를 가진다면 가입할 수 있다.")
+    @DisplayName("사용자는 이메일, 닉네임, 비밀번호의 정보를 가진다면 생성할 수 있다.")
     void createUser(){
         // given
         String email = "hyo37009@gmail.com";
@@ -22,6 +22,20 @@ class UserTest {
 
         //then
         assertThat(newUser).isNotNull();
+    }
+
+    @Test
+    @DisplayName("닉네임을 수정할 수 있다.")
+    void changeNickname(){
+        // given
+        User user = new User("hyo37009@gmail.com", "nickname", "password");
+
+        // when
+        String newNickname = "itsMe";
+        user.updateNickname(newNickname);
+
+        //then
+        assertThat(user.getNickname()).isEqualTo(newNickname);
     }
 
 }
