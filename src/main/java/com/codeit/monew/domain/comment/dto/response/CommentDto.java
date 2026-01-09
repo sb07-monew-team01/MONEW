b4e4663d44c2dfa4aef7e9d51d601623e6229537
dogyeom <sprfv@naver.com>
2026-01-09 17:49:55 +0900

    feat: 댓글 등록 요청 및 응답 DTO 생성

---
+++ src/main/java/com/codeit/monew/domain/comment/dto/response/CommentDto.java
@@ -0,0 +1,15 @@
+package com.codeit.monew.domain.comment.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CommentDto(
+        UUID id,
+        UUID articleId,
+        UUID userId,
+        String userNickname,
+        String content,
+        Long likeCount,
+        Boolean likedByMe,
+        LocalDateTime createdAt
+) { }
