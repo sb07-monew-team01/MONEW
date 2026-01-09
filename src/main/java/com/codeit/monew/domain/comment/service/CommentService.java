dcd4e66e42a40850add79c128c042574bbf0126a
dogyeom <sprfv@naver.com>
2026-01-09 17:52:47 +0900

    feat: 댓글 등록 서비스 로직 구현

---
+++ src/main/java/com/codeit/monew/domain/comment/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.codeit.monew.domain.comment.service;
+
+import com.codeit.monew.domain.comment.dto.request.CommentRegisterRequest;
+import com.codeit.monew.domain.comment.dto.response.CommentDto;
+
+public interface CommentService {
+    CommentDto createComment(CommentRegisterRequest request);
+
+}
--- src/main/java/com/codeit/monew/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/comment/service/CommentServiceImpl.java
@@ -1,4 +1,44 @@
 package com.codeit.monew.domain.comment.service;
 
-public class CommentServiceImpl {
+import com.codeit.monew.domain.article.entity.Article;
+import com.codeit.monew.domain.article.repository.ArticleRepository;
+import com.codeit.monew.domain.comment.dto.request.CommentRegisterRequest;
+import com.codeit.monew.domain.comment.dto.response.CommentDto;
+import com.codeit.monew.domain.comment.entity.Comment;
+import com.codeit.monew.domain.comment.repository.CommentRepository;
+import com.codeit.monew.domain.user.User;
+import com.codeit.monew.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
+
+    @Override
+    public CommentDto createComment(CommentRegisterRequest request) {
+        User user = userRepository.findById(request.userId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        Article article = articleRepository.findById(request.articleId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 기사입니다."));
+
+        Comment comment = new Comment(user, article, request.content());
+        Comment saved = commentRepository.save(comment);
+
+        return new CommentDto(
+                saved.getId(),
+                saved.getArticle().getId(),
+                saved.getUser().getId(),
+                saved.getUser().getNickname(),
+                saved.getContent(),
+                0L,
+                false,
+                saved.getCreatedAt()
+        );
+    }
 }
