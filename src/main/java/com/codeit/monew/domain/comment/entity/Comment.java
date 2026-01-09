311745ff3a9ea900fbcf00a0b9f3084361c64599
dogyeom <sprfv@naver.com>
2026-01-09 13:47:50 +0900

    feat: 댓글 Entity 추가 및 JPA Auditing 설정 추가

---
+++ src/main/java/com/codeit/monew/domain/comment/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.codeit.monew.domain.comment.entity;
+
+import com.codeit.monew.domain.BaseUpdatableEntity;
+import com.codeit.monew.domain.article.entity.Article;
+import com.codeit.monew.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseUpdatableEntity {
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = false)
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+}
