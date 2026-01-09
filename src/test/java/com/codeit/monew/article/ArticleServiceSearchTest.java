92c501316fea15610dc280cda36528210e8c31e1
gnara0719 <ppjw254236@gmail.com>
2026-01-09 11:18:26 +0900

    test: 키워드와 출처로 기사 조회 기능 추가 (RED)
    - 키워드와 출처로 조회할 수 있다.
    - 관련 메서드 부재로 인한 오류

--- src/test/java/com/codeit/monew/article/ArticleServiceSearchTest.java
+++ src/test/java/com/codeit/monew/article/ArticleServiceSearchTest.java
@@ -3,6 +3,7 @@ package com.codeit.monew.article;
 import com.codeit.monew.domain.article.dto.mapper.ArticleMapper;
 import com.codeit.monew.domain.article.dto.response.ArticleDto;
 import com.codeit.monew.domain.article.entity.Article;
+import com.codeit.monew.domain.article.entity.ArticleSource;
 import com.codeit.monew.domain.article.repository.ArticleRepository;
 import com.codeit.monew.domain.article.service.ArticleService;
 import com.codeit.monew.domain.article.service.ArticleServiceImpl;
@@ -37,29 +38,30 @@ public class ArticleServiceSearchTest {
     class Search {
 
         @Test
-        @DisplayName("검색어가 기사 제목 또는 요약에 포함되면 조회할 수 있다.")
+        @DisplayName("검색어와 출처가 일치하는 기사를 조회한다.")
         void titleOrSummaryContainingKeyword() {
             // given
             Article article1 = Article.builder()
-                    .title("축구리그")
+                    .title("축구 리그")
+                    .source(ArticleSource.NAVER)
                     .build();
             Article article2 = Article.builder()
-                    .summary("축구재밌다")
+                    .summary("축구는 재밌다")
+                    .source(ArticleSource.CHOSUN)
                     .build();
             String keyword = "축구";
+            ArticleSource source = ArticleSource.NAVER;
 
-            when(articleRepository.findByKeyword(keyword))
-                    .thenReturn(List.of(article1, article2));
+            when(articleRepository.findByKeywordAndSource(keyword, source))
+                    .thenReturn(List.of(article1));
 
             // when
-            List<ArticleDto> articles = articleService.searchByKeyword(keyword);
+            List<ArticleDto> articles = articleService.searchByKeyword(keyword, source);
 
             // then
             verify(articleRepository, times(1)).findByKeyword(any());
-            verify(articleMapper, times(2)).toDto(any(Article.class));
-            assertThat(articles).hasSize(2);
-            assertThat(articles).extracting(ArticleDto::title).contains("축구리그");
-            assertThat(articles).extracting(ArticleDto::summary).contains("축구재밌다");
+            assertThat(articles).hasSize(1);
+            assertThat(articles).extracting(ArticleDto::title).contains("축구 리그");
         }
     }
 }
