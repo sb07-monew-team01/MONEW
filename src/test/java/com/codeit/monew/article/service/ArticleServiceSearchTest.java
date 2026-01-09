03ea021c14cb3f1d46a55abc673c00e3b68dd417
gnara0719 <ppjw254236@gmail.com>
2026-01-09 22:16:25 +0900

    test: 날짜 범위 내의 기사를 조회하는 기능 추가 (RED)
    - 서비스가 매개변수로 날짜를 받지 않음
    - DTO에 발행날짜가 없음
    - 날짜로 조회하는 쿼리 없음
    - 이러한 이유로 테스트 실패

--- src/test/java/com/codeit/monew/article/service/ArticleServiceSearchTest.java
+++ src/test/java/com/codeit/monew/article/service/ArticleServiceSearchTest.java
@@ -15,6 +15,9 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,35 +40,41 @@ public class ArticleServiceSearchTest {
     class Search {
 
         @Test
-        @DisplayName("검색어와 선택한 여러 출처가 일치하는 기사를 조회한다.")
+        @DisplayName("검색어와 선택한 여러 출처가 일치하는 기사를 지정한 날짜 범위 내에서 조회한다.")
         void titleOrSummaryContainingKeyword() {
             // given
             Article article1 = Article.builder()
                     .title("네이버 뉴스")
                     .source(ArticleSource.NAVER)
+                    .publishDate(LocalDateTime.now())
                     .build();
             Article article2 = Article.builder()
                     .title("조선 뉴스")
                     .source(ArticleSource.CHOSUN)
+                    .publishDate(LocalDateTime.now().minusDays(1))
                     .build();
             Article article3 = Article.builder()
                     .title("한경 뉴스")
                     .source(ArticleSource.HANKYUNG)
+                    .publishDate(LocalDateTime.now().minusDays(2))
                     .build();
 
             String keyword = "뉴스";
             List<ArticleSource> sources = List.of(ArticleSource.NAVER, ArticleSource.HANKYUNG);
+            LocalDateTime publishDateTo = LocalDate.now().atTime(LocalTime.MIDNIGHT);
+            LocalDateTime publishDateFrom = publishDateTo.minusDays(7);
 
-            when(articleRepository.findByKeywordAndSource(keyword, sources))
-                    .thenReturn(List.of(article1, article3));
+            when(articleRepository.findByKeywordAndSource(keyword, sources, publishDateFrom, publishDateTo))
+                    .thenReturn(List.of(article3));
 
             // when
-            List<ArticleDto> articles = articleService.searchByKeyword(keyword, sources);
+            List<ArticleDto> articles = articleService.searchByKeyword(keyword, sources, publishDateFrom, publishDateTo);
 
             // then
-            verify(articleRepository, times(1)).findByKeywordAndSource(any(), any());
-            assertThat(articles).hasSize(2);
-            assertThat(articles).extracting(ArticleDto::source).containsExactly("NAVER", "HANKYUNG");
+            assertThat(articles).hasSize(1);
+            assertThat(articles).extracting(ArticleDto::source).containsExactly("HANKYUNG");
+            assertThat(articles.get(0).publishDate())
+                    .isAfterOrEqualTo(publishDateFrom).isBeforeOrEqualTo(publishDateTo);
         }
     }
 }
