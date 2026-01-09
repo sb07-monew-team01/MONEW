6d0f82803538a74416b42621c4dcc0da71ccadba
ChoiJiHye950 <principle950@daum.net>
2026-01-09 15:17:23 +0900

    feat : 관심사 엔티티 변경 및 JPA 매핑 추가 (GREEN)
    - 관심사 엔티티에 JPA 어노테이션 추가하여 영속성 매핑 처리
    - BaseUpdatableEntity 상속하여 공통 필드 활용
    - Lombok 어노테이션으로 기본 생성자 및 접근 제어 설정
    - Transient 키워드로 비영속 필드인 keywords 지정

--- src/main/java/com/codeit/monew/domain/interest/entity/Interest.java
+++ src/main/java/com/codeit/monew/domain/interest/entity/Interest.java
@@ -1,14 +1,26 @@
 package com.codeit.monew.domain.interest.entity;
 
+import com.codeit.monew.domain.BaseUpdatableEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class Interest {
-    private final String name;
-    private final List<String> keywords;
+@Entity
+@Table(name = "interests")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Interest extends BaseUpdatableEntity {
+
+    private String name;
+
+    @Transient
+    private List<String> keywords;
 
     public Interest(String name, List<String> keywords) {
         this.name = name;
