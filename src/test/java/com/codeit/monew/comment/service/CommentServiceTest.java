28cc6acb5bd70d4952b173f4d1f221ce85d246d4
dogyeom <sprfv@naver.com>
2026-01-09 18:00:34 +0900

    feat: 댓글 등록 기능 구현 및 테스트 통과 (GREEN)

---
+++ src/test/java/com/codeit/monew/comment/service/CommentServiceTest.java
@@ -0,0 +1,86 @@
+package com.codeit.monew.comment.service;
+
+import com.codeit.monew.domain.article.entity.Article;
+import com.codeit.monew.domain.article.entity.ArticleSource;
+import com.codeit.monew.domain.article.repository.ArticleRepository;
+import com.codeit.monew.domain.comment.dto.request.CommentRegisterRequest;
+import com.codeit.monew.domain.comment.dto.response.CommentDto;
+import com.codeit.monew.domain.comment.entity.Comment;
+import com.codeit.monew.domain.comment.repository.CommentRepository;
+import com.codeit.monew.domain.comment.service.CommentServiceImpl;
+import com.codeit.monew.domain.user.User;
+import com.codeit.monew.domain.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("CommentService Test")
+public class CommentServiceTest {
+
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private ArticleRepository articleRepository;
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private CommentServiceImpl commentService;
+
+    private UUID userId;
+    private UUID articleId;
+
+    @Nested
+    @DisplayName("createComment()")
+    class CreateComment {
+
+        @Test
+        @DisplayName("should create comment when user and article exist")
+        void createComment_success() {
+            // given
+            String content = "test";
+
+            User user = new User("test@email.com","nick","1234");
+            Article article = new Article(
+                    ArticleSource.NAVER,
+                    "www.naver.com/123",
+                    "제목입니다.", LocalDateTime.now(),
+                    "요약입니다.",
+                    null
+            );
+
+            CommentRegisterRequest request = new CommentRegisterRequest(userId, articleId, content);
+            Comment savedComment = new Comment(user, article, content);
+
+            when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+            when(articleRepository.findById(articleId)).thenReturn(Optional.of(article));
+            when(commentRepository.save(any(Comment.class))).thenReturn(savedComment);
+
+            // when
+            CommentDto response = commentService.createComment(request);
+
+            // then
+            assertThat(response).isNotNull();
+            assertThat(response.content()).isEqualTo(content);
+            verify(userRepository).findById(userId);
+            verify(articleRepository).findById(articleId);
+            verify(commentRepository).save(any(Comment.class));
+        }
+    }
+
+}
