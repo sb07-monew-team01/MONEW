4a0348c33991c451a2e617361db4c94534707420
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-09 13:41:38 +0900

    feat: Article 생성 중복 SourceUrl 테스트 (RED)
    - TextFixture 작성
    - ArticleRepository에 existsBySourceUrl 메소드 추가

--- src/main/java/com/codeit/monew/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/codeit/monew/domain/article/repository/ArticleRepository.java
@@ -1,7 +1,6 @@
 package com.codeit.monew.domain.article.repository;
 
 import com.codeit.monew.domain.article.entity.Article;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +15,6 @@ public interface ArticleRepository extends JpaRepository<Article, UUID> {
     @Query("SELECT a FROM Article a " +
             "WHERE a.title LIKE %:keyword% OR a.summary Like %:keyword%")
     List<Article> findByKeyword(@Param("keyword") String keyword);
+
+    boolean existsBySourceUrl(String sourceUrl);
 }
--- src/test/java/com/codeit/monew/article/service/ArticleServiceCreateTest.java
+++ src/test/java/com/codeit/monew/article/service/ArticleServiceCreateTest.java
@@ -1,8 +1,9 @@
 package com.codeit.monew.article.service;
 
+import com.codeit.monew.article.fixture.ArticleCreateRequestFixture;
+import com.codeit.monew.article.fixture.ArticleFixture;
 import com.codeit.monew.domain.article.dto.request.ArticleCreateRequest;
 import com.codeit.monew.domain.article.entity.Article;
-import com.codeit.monew.domain.article.entity.ArticleSource;
 import com.codeit.monew.domain.article.repository.ArticleRepository;
 import com.codeit.monew.domain.article.service.ArticleServiceImpl;
 import org.junit.jupiter.api.DisplayName;
@@ -13,11 +14,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,30 +38,32 @@ public class ArticleServiceCreateTest {
         @DisplayName("기사 정보와 관심사 Id가 들어오면 Article를 저장한다.")
         void createArticle() {
             //given
-            ArticleCreateRequest request = new ArticleCreateRequest(
-                    ArticleSource.NAVER,
-                    "http://target.com",
-                    "test-title",
-                    LocalDateTime.now(),
-                    "test summary"
-            );
-
-            Article article = Article.builder()
-                    .source(request.source())
-                    .sourceUrl(request.sourceUrl())
-                    .title(request.title())
-                    .publishDate(request.publishDate())
-                    .summary(request.summary())
-                    .build();
-
+            ArticleCreateRequest request = ArticleCreateRequestFixture.createDefault();
+            Article article = ArticleFixture.createEntity(request);
             UUID interestId =  UUID.randomUUID();
+
             // when
             articleService.createArticle(request, interestId);
 
             //then
             then(articleRepository).should(times(1)).save(any(Article.class));
         }
+
+        @Test
+        @DisplayName("기사 정보 중 이미 존재하는 SourceUrl이면 저장하지 않는다.")
+        void createArticle_duplicatedSourceUrl_thenFail() {
+            // given
+            ArticleCreateRequest request = ArticleCreateRequestFixture.createDefault();
+
+            // 기존에 존재하는 sourceURl을 가진 Article
+            given(articleRepository.existsBySourceUrl(request.sourceUrl())).willReturn(true);
+            UUID interestId =  UUID.randomUUID();
+
+            // when
+            articleService.createArticle(request, interestId);
+
+            then(articleRepository).should(never()).save(any(Article.class));
+
+        }
     }
-
-
 }
