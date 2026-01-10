0c7d5c18abe17058ab3abae4d973e8ee2c2c1525
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-10 22:57:54 +0900

    feat: Article Matcher 기능 추가(GREEN)

--- src/main/java/com/codeit/monew/domain/article/matcher/ArticleMatcherImpl.java
+++ src/main/java/com/codeit/monew/domain/article/matcher/ArticleMatcherImpl.java
@@ -13,9 +13,12 @@ public class ArticleMatcherImpl implements ArticleMatcher {
     @Override
     public boolean match(ArticleCreateRequest articleCreateRequest, List<Interest> interests) {
         String title = articleCreateRequest.title().toLowerCase();
+        String summary =  articleCreateRequest.summary().toLowerCase();
 
         return interests.stream()
-                .anyMatch(interest -> matchInterest(interest, title));
+                .anyMatch(interest -> matchInterest(interest, title)) ||
+                interests.stream()
+                        .anyMatch(interest -> matchInterest(interest, summary));
 
 
     }
@@ -30,5 +33,16 @@ public class ArticleMatcherImpl implements ArticleMatcher {
 
         return searchItems.stream()
                 .anyMatch(keyword -> keyword.trim().toLowerCase().equals(title));
+    }
+
+    private boolean matchInterestWithContent(Interest interest, String summary) {
+        List<String> searchItems = new ArrayList<>();
+        searchItems.add(interest.getName().toLowerCase().trim());
+
+        interest.getKeywords()
+                .forEach(keyword -> searchItems.add(keyword.trim().toLowerCase()));
+
+        return searchItems.stream()
+                .anyMatch(keyword -> keyword.trim().toLowerCase().contains(summary));
     }
 }
