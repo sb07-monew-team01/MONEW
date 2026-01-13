d5e9632f4f0c01f66d2164540f89daaa128c9ed6
sinjawon <qlqhroal@naver.com>
2026-01-13 14:20:50 +0900

    feat : delete 테스트 코드 추가

--- src/main/java/com/codeit/monew/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/monew/domain/notification/repository/NotificationRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
     //영속성 확인 후 -> delete 하고 -> 영속성컨텍스트 비워
