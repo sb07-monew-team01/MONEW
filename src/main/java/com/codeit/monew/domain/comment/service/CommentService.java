ead45522d3720d12764cdf7f2509146ef58223d8
dogyeom <sprfv@naver.com>
2026-01-14 23:13:57 +0900

    fix: delete 반환 타입 CommentDto에서 void로 변경, 실패 테스트 코드 수정

--- src/main/java/com/codeit/monew/domain/comment/service/CommentService.java
+++ src/main/java/com/codeit/monew/domain/comment/service/CommentService.java
@@ -2,10 +2,8 @@ package com.codeit.monew.domain.comment.service;
 
 import com.codeit.monew.domain.comment.dto.request.CommentRegisterRequest;
 import com.codeit.monew.domain.comment.dto.response.CommentDto;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
-@Transactional
 public interface CommentService {
     CommentDto create(CommentRegisterRequest request);
     CommentDto delete(UUID commentId);
