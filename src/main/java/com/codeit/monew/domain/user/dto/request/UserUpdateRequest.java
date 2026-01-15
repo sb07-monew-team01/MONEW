d348a7c18602f9829f67486745eae8bac58fd5c0
hyo37009 <hyo37009@gmail.com>
2026-01-15 11:24:02 +0900

    refactor: UserUpdateRequest 유효성 검증 어노테이션 수정
    - @NotNull을 @NotBlank로 변경
    - 유저 아이디 및 닉네임의 공백 검증 강화

--- src/main/java/com/codeit/monew/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/codeit/monew/domain/user/dto/request/UserUpdateRequest.java
@@ -1,15 +1,15 @@
 package com.codeit.monew.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
 public record UserUpdateRequest(
-        @NotNull(message = "유저 아이디는 필수입니다.")
+        @NotBlank(message = "유저 아이디는 필수입니다.")
         UUID userId,
 
         // TODO 닉네임 유효성 검사
-        @NotNull(message = "유저 닉네임은 필수입니다.")
+        @NotBlank(message = "유저 닉네임은 필수입니다.")
         String newNickname
 ) {
 }
