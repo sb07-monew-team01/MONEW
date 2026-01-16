643b75aee91cb42ebc97b9f6c76ff502e235e982
ChoiJiHye950 <principle950@daum.net>
2026-01-17 01:12:34 +0900

    feat : Interest 키워드 검증 로직 추가 (GREEN)
    - 키워드가 null일 경우 예외 발생하도록 처리
    - 키워드 목록이 비어있을 경우 예외 발생 로직 추가
    - 키워드 개수가 10개를 초과하면 예외 발생하도록 설정
    - 키워드 중복 시 예외가 발생하도록 검증 메소드 추가

--- src/main/java/com/codeit/monew/domain/interest/entity/Interest.java
+++ src/main/java/com/codeit/monew/domain/interest/entity/Interest.java
@@ -1,7 +1,9 @@
 package com.codeit.monew.domain.interest.entity;
 
 import com.codeit.monew.domain.BaseUpdatableEntity;
+import com.codeit.monew.domain.interest.exception.KeywordValidException;
 import com.codeit.monew.domain.interestkeyword.entity.InterestKeyword;
+import com.codeit.monew.global.enums.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -29,9 +31,14 @@ public class Interest extends BaseUpdatableEntity {
 
     //Constructors
     public Interest(String name, List<String> keywords) {
+        if(keywords == null){
+            throw new KeywordValidException(ErrorCode.INTEREST_NULL_KEYWORD);
+        }
+
         this.name = name;
         this.keywords = new ArrayList<>();
         keywords.forEach(this::addKeyword);
+        checkKeyword(keywords);
     }
 
     //Methods
@@ -44,5 +51,25 @@ public class Interest extends BaseUpdatableEntity {
 
     private void addKeyword(String keyword) {
         this.keywords.add(new InterestKeyword(this, keyword));
+    }
+
+    //키워드 검증 메소드 분리
+    private void checkKeyword(List<String> keywords) {
+        if(keywords.isEmpty()){
+            throw new KeywordValidException(ErrorCode.INTEREST_EMPTY_KEYWORD);
+        }
+        if(keywords.size() > 10){
+            throw new KeywordValidException(ErrorCode.TOO_MANY_KEYWORD);
+        }
+        checkDuplicateKeyword(keywords);
+    }
+
+    //같은 관심사 내에 중복 키워드가 있는지 체크하고, 있으면 예외를 발생시키는 메소드
+    private void checkDuplicateKeyword(List<String> keywords) {
+        if(keywords.size() != keywords.stream().distinct().count()){
+            throw new KeywordValidException(
+                    ErrorCode.INTEREST_KEYWORD_DUPLICATE
+            );
+        }
     }
 }
