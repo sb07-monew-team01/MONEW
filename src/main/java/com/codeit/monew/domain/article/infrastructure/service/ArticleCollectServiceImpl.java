38ddc4eacad184a57460cad44329bda4b6a43001
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-17 13:36:11 +0900

    feat: 수집된 기사 저장 기능 (Green) - 데이터베이스에 저장된 관심사를 기반으로 collector를 실행시킨다.

--- src/main/java/com/codeit/monew/domain/article/infrastructure/service/ArticleCollectServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/article/infrastructure/service/ArticleCollectServiceImpl.java
@@ -1,12 +1,15 @@
 package com.codeit.monew.domain.article.infrastructure.service;
 
 import com.codeit.monew.domain.article.dto.mapper.ArticleMapper;
+import com.codeit.monew.domain.article.dto.request.ArticleCreateRequest;
 import com.codeit.monew.domain.article.infrastructure.ArticleCollector;
 import com.codeit.monew.domain.article.repository.ArticleRepository;
+import com.codeit.monew.domain.interest.entity.Interest;
 import com.codeit.monew.domain.interest.repository.InterestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,6 +23,15 @@ public class ArticleCollectServiceImpl implements ArticleCollectService {
 
     @Override
     public void collectAndSave() {
-        interestRepository.findAll();
+        List<Interest> interests = interestRepository.findAll();
+        if (interests.isEmpty()) {
+            return;
+        }
+
+        List<ArticleCreateRequest> articleCreateRequests = new ArrayList<>();
+        for(ArticleCollector articleCollector : articleCollectors) {
+            List<ArticleCreateRequest> articles = articleCollector.collect(interests);
+        }
+
     }
 }
