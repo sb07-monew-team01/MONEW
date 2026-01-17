9c48ab5797c3fcb53f5597d44d08f3a4f3fc21e6
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-17 19:02:59 +0900

    remove: 더이상 사용하지 않는 CreateArticle 메서드 삭제

--- src/main/java/com/codeit/monew/domain/article/service/ArticleService.java
+++ src/main/java/com/codeit/monew/domain/article/service/ArticleService.java
@@ -1,18 +1,13 @@
 package com.codeit.monew.domain.article.service;
 
-import com.codeit.monew.global.dto.PageResponse;
-import com.codeit.monew.domain.article.dto.request.ArticleCreateRequest;
 import com.codeit.monew.domain.article.dto.request.ArticleSearchRequest;
 import com.codeit.monew.domain.article.dto.response.ArticleDto;
-import com.codeit.monew.domain.interest.entity.Interest;
+import com.codeit.monew.global.dto.PageResponse;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ArticleService {
     PageResponse<ArticleDto> searchByKeyword(ArticleSearchRequest searchRequest);
-
-    void createArticle(ArticleCreateRequest request, List<Interest>  interests);
 
     ArticleDto searchById(UUID articleId);
 }
