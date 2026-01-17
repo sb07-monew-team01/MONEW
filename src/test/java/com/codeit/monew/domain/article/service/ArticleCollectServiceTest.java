522de0d0b7eeb982dada93c0c8b500f9751674d4
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-17 13:36:00 +0900

    feat: 수집된 기사 저장 기능 (RED) - 데이터베이스에 저장된 관심사를 기반으로 collector를 실행시킨다.

--- src/test/java/com/codeit/monew/domain/article/service/ArticleCollectServiceTest.java
+++ src/test/java/com/codeit/monew/domain/article/service/ArticleCollectServiceTest.java
@@ -1,6 +1,8 @@
 package com.codeit.monew.domain.article.service;
 
 import com.codeit.monew.domain.article.dto.mapper.ArticleMapper;
+import com.codeit.monew.domain.article.dto.request.ArticleCreateRequest;
+import com.codeit.monew.domain.article.fixture.ArticleCreateRequestFixture;
 import com.codeit.monew.domain.article.fixture.InterestFixture;
 import com.codeit.monew.domain.article.infrastructure.ArticleCollector;
 import com.codeit.monew.domain.article.infrastructure.service.ArticleCollectServiceImpl;
@@ -19,6 +21,7 @@ import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class ArticleCollectServiceTest {
@@ -41,29 +44,51 @@ public class ArticleCollectServiceTest {
     @InjectMocks
     ArticleCollectServiceImpl articleCollectService;
 
+    private List<Interest> interests;
+
     @BeforeEach
     void setUp() {
         articleCollectService = new ArticleCollectServiceImpl(
                 List.of(articleCollector1, articleCollector2),
                 articleRepository,
+                interestRepository,
                 articleMapper
+        );
+
+        interests = InterestFixture.createMultiple(
+                InterestFixture.create("축구", List.of("손흥민", "메시")),
+                InterestFixture.create("경제", List.of("코인"))
         );
     }
 
     @Test
     @DisplayName("저장된 모든 Interests를 조회할 수 있다.")
-    void collectAllNews_FindAllInterests() {
+    void collectAllArticle_FindAllInterests() {
         // given
-        List<Interest> interests = InterestFixture.createMultiple(
-                InterestFixture.create("축구", List.of("손흥민", "메시")),
-                InterestFixture.create("경제", List.of("코인"))
-        );
-
         given(interestRepository.findAll()).willReturn(interests);
 
-        articleCollectService.collectAndSave(interests);
+        // when
+        articleCollectService.collectAndSave();
 
+        // then
         then(interestRepository).should().findAll();
+    }
+
+    @Test
+    @DisplayName("Interest가 1개 이상 존재한다면 기사를 수집한다.")
+    void collect_when_isNotEmpty_start_collect() {
+        // given
+        given(interestRepository.findAll()).willReturn(interests);
+        ArticleCreateRequest articleCreateRequest1 = ArticleCreateRequestFixture.createWithTitleAndSummary("제목1", "기사내용입니다1");
+        ArticleCreateRequest articleCreateRequest2 = ArticleCreateRequestFixture.createWithTitleAndSummary("제목2", "기사내용입니다2");
+        given(articleCollector1.collect(interests)).willReturn(List.of(articleCreateRequest1, articleCreateRequest2));
+        given(articleCollector2.collect(interests)).willReturn(List.of(articleCreateRequest1, articleCreateRequest2));
+
+        // when
+        articleCollectService.collectAndSave();
+
+        then(articleCollector1).should(times(1)).collect(interests);
+        then(articleCollector2).should(times(1)).collect(interests);
     }
 
 }
