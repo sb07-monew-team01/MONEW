239abe295d97867ed953eece8dbae1b99a51e924
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-17 12:05:09 +0900

    기사 수집 및 저장 서비스 (GREEN)

--- src/main/java/com/codeit/monew/domain/article/infrastructure/service/ArticleCollectService.java
+++ src/main/java/com/codeit/monew/domain/article/infrastructure/service/ArticleCollectService.java
@@ -1,10 +1,6 @@
 package com.codeit.monew.domain.article.infrastructure.service;
 
-import com.codeit.monew.domain.interest.entity.Interest;
-
-import java.util.List;
-
 public interface ArticleCollectService {
 
-    void collectAndSave(List<Interest> interests);
+    void collectAndSave();
 }
--- src/main/java/com/codeit/monew/domain/article/infrastructure/service/ArticleCollectServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/article/infrastructure/service/ArticleCollectServiceImpl.java
@@ -1,6 +1,9 @@
 package com.codeit.monew.domain.article.infrastructure.service;
 
-import com.codeit.monew.domain.interest.entity.Interest;
+import com.codeit.monew.domain.article.dto.mapper.ArticleMapper;
+import com.codeit.monew.domain.article.infrastructure.ArticleCollector;
+import com.codeit.monew.domain.article.repository.ArticleRepository;
+import com.codeit.monew.domain.interest.repository.InterestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +13,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleCollectServiceImpl implements ArticleCollectService {
 
+    private final List<ArticleCollector> articleCollectors;
+    private final ArticleRepository articleRepository;
+    private final InterestRepository interestRepository;
+    private final ArticleMapper articleMapper;
+
     @Override
-    public void collectAndSave(List<Interest> interests) {
+    public void collectAndSave() {
+        interestRepository.findAll();
     }
 }
