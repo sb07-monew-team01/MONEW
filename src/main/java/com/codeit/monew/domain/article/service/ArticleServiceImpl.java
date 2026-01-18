a1a073423e67f5ce1586e8c77acb7aab308af514
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-18 16:32:52 +0900

    feat: 기사 논리 삭제 기능 추가(GREEN) - 이미 삭제된 기사 예외 처리

--- src/main/java/com/codeit/monew/domain/article/service/ArticleServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/article/service/ArticleServiceImpl.java
@@ -113,6 +113,10 @@ public class ArticleServiceImpl implements ArticleService {
         Article article = articleRepository.findById(articleId)
                 .orElseThrow(() -> new ArticleNotFoundException(articleId));
 
+        if(article.getDeletedAt() != null){
+            throw new ArticleNotFoundException(articleId);
+        }
+
         article.softDelete();
     }
 }
