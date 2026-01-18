99d65734d77130b660d6646c1f0f21bd520ef4e6
gnara0719 <ppjw254236@gmail.com>
2026-01-18 12:08:30 +0900

    feat: 특정 사용자의 다중 기사 조회 여부 확인 쿼리 추가
    - `ArticleViewRepository`에 사용자별 다중 기사 조회 여부 확인 메서드 추가 (`findViewedByUserIdAndArticleId`)
    - JPQL 활용해 특정 사용자와 다중 기사 ID 매칭 데이터 반환
    - 관련 파라미터 매핑 및 쿼리 구현

--- src/main/java/com/codeit/monew/domain/articleView/repository/ArticleViewRepository.java
+++ src/main/java/com/codeit/monew/domain/articleView/repository/ArticleViewRepository.java
@@ -2,12 +2,20 @@ package com.codeit.monew.domain.articleView.repository;
 
 import com.codeit.monew.domain.articleView.entity.ArticleView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface ArticleViewRepository extends JpaRepository<ArticleView, UUID> {
 
     Boolean existsByUserIdAndArticleId(UUID userId, UUID articleId);
+
+    @Query("select av.article.id from ArticleView av where av.user.id = :userId and av.article.id IN :article_id ")
+    Set<UUID> findViewedByUserIdAndArticleId(@Param("user_id") UUID userId,
+                                             @Param("article_ids") List<UUID> articleIds);
 }
