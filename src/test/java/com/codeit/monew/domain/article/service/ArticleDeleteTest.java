f019508371b236da568e72e5931ad5c158da0bc0
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-18 18:10:35 +0900

    refactor: 테스트코드 사용하지 않는 EntityManager 개선
    -정상 케이스에 findById 추가

--- src/test/java/com/codeit/monew/domain/article/service/ArticleDeleteTest.java
+++ src/test/java/com/codeit/monew/domain/article/service/ArticleDeleteTest.java
@@ -4,7 +4,6 @@ import com.codeit.monew.domain.article.entity.Article;
 import com.codeit.monew.domain.article.exception.ArticleNotFoundException;
 import com.codeit.monew.domain.article.fixture.ArticleFixture;
 import com.codeit.monew.domain.article.repository.ArticleRepository;
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -28,8 +27,6 @@ public class ArticleDeleteTest {
 
     @InjectMocks
     private ArticleServiceImpl articleService;
-
-    private EntityManager entityManager;
 
     @Nested
     @DisplayName("논리 삭제 테스트")
@@ -92,6 +89,8 @@ public class ArticleDeleteTest {
         void hardDelete_success() {
             // given
             UUID articleId = UUID.randomUUID();
+            Article article = ArticleFixture.createDefaultEntity();
+            given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
             willDoNothing().given(articleRepository).deleteById(articleId);
 
             // when
@@ -101,7 +100,22 @@ public class ArticleDeleteTest {
             then(articleRepository).should().deleteById(articleId);
 
         }
+        
+        @Test
+        @DisplayName("존재하지 않는 기사면 삭제할 수 없고 예외를 발생한다.")
+        void hardDelete_nullArticle_fail() {
+            // given
+            UUID articleId = UUID.randomUUID();
+            given(articleRepository.findById(articleId)).willReturn(Optional.empty());
 
+            // when
+            assertThatThrownBy(() -> articleService.hardDelete(articleId))
+                    .isInstanceOf(ArticleNotFoundException.class);
+
+            // then
+            then(articleRepository).should(never()).deleteById(articleId);
+            
+        }
 
 
     }
