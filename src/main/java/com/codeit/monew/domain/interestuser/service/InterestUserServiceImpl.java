d1b35644da89b6282271fd3e65f73b7268a71536
ChoiJiHye950 <principle950@daum.net>
2026-01-18 14:09:06 +0900

    feat : 관심사 조회 실패 시 예외 처리 추가 (GREEN)
    - 존재하지 않는 관심사를 조회할 때 예외 발생 로직 추가
    - InterestNotFoundException 생성 및 적용
    - 명확한 오류 메시지 전달을 위해 ErrorCode 활용

--- src/main/java/com/codeit/monew/domain/interestuser/service/InterestUserServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/interestuser/service/InterestUserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.codeit.monew.domain.interestuser.service;
 
 import com.codeit.monew.domain.interest.entity.Interest;
+import com.codeit.monew.domain.interest.exception.web.InterestNotFoundException;
 import com.codeit.monew.domain.interest.repository.InterestRepository;
 import com.codeit.monew.domain.interestuser.entity.InterestUser;
 import com.codeit.monew.domain.interestuser.repository.InterestUserRepository;
 import com.codeit.monew.domain.user.entity.User;
 import com.codeit.monew.domain.user.exception.UserNotFoundException;
 import com.codeit.monew.domain.user.repository.UserRepository;
+import com.codeit.monew.global.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +27,9 @@ public class InterestUserServiceImpl implements InterestUserService{
     public InterestUser subscribe(UUID userId, UUID interestId) {
         User user = userRepository.findById(userId).orElseThrow(
                 () ->  new UserNotFoundException(userId));
-        Interest interest = interestRepository.findById(interestId).get();
+        Interest interest = interestRepository.findById(interestId).orElseThrow(
+                () -> new InterestNotFoundException(ErrorCode.INTEREST_NOT_FOUND)
+        );
         return interestUserRepository.save(new InterestUser(user, interest));
     }
 }
