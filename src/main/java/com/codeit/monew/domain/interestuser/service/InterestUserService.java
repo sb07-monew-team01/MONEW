c6b606fc64b7b7907f6331fa0c6a411691c1ef6a
ChoiJiHye950 <principle950@daum.net>
2026-01-18 13:58:47 +0900

    feat : 관심사 구독 서비스 구현 (GREEN)
    - 사용자가 관심사를 구독하는 서비스 로직 추가
    - InterestUser 관계를 저장하는 비즈니스 로직 구현
    - 관심사 및 사용자 데이터 검증 로직 통합

---
+++ src/main/java/com/codeit/monew/domain/interestuser/service/InterestUserService.java
@@ -0,0 +1,9 @@
+package com.codeit.monew.domain.interestuser.service;
+
+import com.codeit.monew.domain.interestuser.entity.InterestUser;
+
+import java.util.UUID;
+
+public interface InterestUserService {
+    InterestUser subscribe(UUID userId, UUID interestId);
+}
---
+++ src/main/java/com/codeit/monew/domain/interestuser/service/InterestUserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.codeit.monew.domain.interestuser.service;
+
+import com.codeit.monew.domain.interest.entity.Interest;
+import com.codeit.monew.domain.interest.repository.InterestRepository;
+import com.codeit.monew.domain.interestuser.entity.InterestUser;
+import com.codeit.monew.domain.interestuser.repository.InterestUserRepository;
+import com.codeit.monew.domain.user.entity.User;
+import com.codeit.monew.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class InterestUserServiceImpl implements InterestUserService{
+    private final UserRepository userRepository;
+    private final InterestRepository interestRepository;
+    private final InterestUserRepository interestUserRepository;
+
+    @Override
+    @Transactional
+    public InterestUser subscribe(UUID userId, UUID interestId) {
+        User user = userRepository.findById(userId).get();
+        Interest interest = interestRepository.findById(interestId).get();
+        return interestUserRepository.save(new InterestUser(user, interest));
+    }
+}
