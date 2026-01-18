9ccb0998f6dac23b1a486e91154d5aaef59ee8b3
ChoiJiHye950 <principle950@daum.net>
2026-01-18 14:06:04 +0900

    feat : 사용자 조회 시 예외 처리 추가 (GREEN)
    - 사용자를 id로 조회할 때 존재하지 않는 경우 예외 발생 로직 추가
    - UserNotFoundException 생성 및 적용으로 명확한 에러 처리 제공

--- src/main/java/com/codeit/monew/domain/interestuser/service/InterestUserServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/interestuser/service/InterestUserServiceImpl.java
@@ -5,6 +5,7 @@ import com.codeit.monew.domain.interest.repository.InterestRepository;
 import com.codeit.monew.domain.interestuser.entity.InterestUser;
 import com.codeit.monew.domain.interestuser.repository.InterestUserRepository;
 import com.codeit.monew.domain.user.entity.User;
+import com.codeit.monew.domain.user.exception.UserNotFoundException;
 import com.codeit.monew.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,8 @@ public class InterestUserServiceImpl implements InterestUserService{
     @Override
     @Transactional
     public InterestUser subscribe(UUID userId, UUID interestId) {
-        User user = userRepository.findById(userId).get();
+        User user = userRepository.findById(userId).orElseThrow(
+                () ->  new UserNotFoundException(userId));
         Interest interest = interestRepository.findById(interestId).get();
         return interestUserRepository.save(new InterestUser(user, interest));
     }
