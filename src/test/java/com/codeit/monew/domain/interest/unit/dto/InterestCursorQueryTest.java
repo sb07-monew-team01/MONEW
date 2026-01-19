3881aa236abafb8cda82ef2fd732ff8e248f23d0
ChoiJiHye950 <principle950@daum.net>
2026-01-19 19:18:04 +0900

    test : InterestCursorQuery 테스트 추가
    - InterestCursorQuery의 기본값 처리 로직 테스트 케이스 작성
    - 정렬 조건에 따른 validate 메서드 검증 테스트 포함
    - 다양한 시나리오를 다루는 단위 테스트 추가

---
+++ src/test/java/com/codeit/monew/domain/interest/unit/dto/InterestCursorQueryTest.java
@@ -0,0 +1,116 @@
+package com.codeit.monew.domain.interest.unit.dto;
+
+import com.codeit.monew.domain.interest.dto.InterestCursorQuery;
+import com.codeit.monew.domain.interest.vo.InterestOrderBy;
+import com.codeit.monew.domain.interest.vo.SortDirection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class InterestCursorQueryTest {
+    @Nested
+    @DisplayName("기본값 셋팅")
+    class DefaultValueTest{
+        @Test
+        @DisplayName("성공: null 입력 시 기본값 설정이 잘 된다")
+        void defaultValues(){
+            // given
+            InterestOrderBy orderBy = null;
+            SortDirection direction = null;
+            Integer limit = null;
+
+            // when
+            InterestCursorQuery query = new InterestCursorQuery(
+                    orderBy,
+                    direction,
+                    null,
+                    null,
+                    null,
+                    limit
+            );
+
+            // then
+            assertThat(query.orderBy()).isEqualTo(InterestOrderBy.NAME);
+            assertThat(query.direction()).isEqualTo(SortDirection.ASC);
+            assertThat(query.limit()).isEqualTo(50);
+        }
+    }
+
+    @Nested
+    @DisplayName("정렬 validate 테스트")
+    class ValidateTest{
+        @Test
+        @DisplayName("정상적인 NAME 정렬 쿼리는 validate 통과한다.")
+        void validateNameCursorSuccess() {
+            // given
+            InterestCursorQuery query = new InterestCursorQuery(
+                    InterestOrderBy.NAME,
+                    SortDirection.ASC,
+                    "cursorName",
+                    null,
+                    null,
+                    10
+            );
+
+
+            // when & then
+            assertThatCode(query::validate)
+                    .doesNotThrowAnyException(); // 예외가 발생하지 않아야 함
+        }
+
+        @Test
+        @DisplayName("NAME 정렬에서 subscriberCountCursor 사용 시 예외 발생")
+        void validateNameCursorFail() {
+            // given
+            InterestCursorQuery query = new InterestCursorQuery(
+                    InterestOrderBy.NAME,
+                    SortDirection.ASC,
+                    null,
+                    5L,
+                    null,
+                    10
+            );
+
+            // when & then
+            assertThatThrownBy(query::validate)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("정상적인 SUBSCRIBER_COUNT 정렬 쿼리는 validate 통과")
+        void validateSubscriberCountCursorSuccess() {
+            // given
+            InterestCursorQuery query = new InterestCursorQuery(
+                    InterestOrderBy.SUBSCRIBER_COUNT,
+                    SortDirection.DESC,
+                    null,
+                    10L,
+                    null,
+                    10
+            );
+
+            // when & then
+            query.validate(); // 예외 없음
+        }
+
+        @Test
+        @DisplayName("SUBSCRIBER_COUNT 정렬에서 nameCursor 사용 시 예외 발생")
+        void validateSubscriberCountCursorFail() {
+            // given
+            InterestCursorQuery query = new InterestCursorQuery(
+                    InterestOrderBy.SUBSCRIBER_COUNT,
+                    SortDirection.DESC,
+                    "nameCursor",
+                    null,
+                    null,
+                    10
+            );
+
+            // when & then
+            assertThatThrownBy(query::validate)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
