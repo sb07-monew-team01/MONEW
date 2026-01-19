37765041db7f5c61be8c4f3363b8c13ad842c9a2
dogyeom <sprfv@naver.com>
2026-01-19 22:51:08 +0900

    feat: 컨트롤러 작성

--- src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
@@ -1,16 +1,22 @@
 package com.codeit.monew.domain.comment.controller;
 
+import com.codeit.monew.domain.comment.dto.request.CommentOrderBy;
 import com.codeit.monew.domain.comment.dto.request.CommentRegisterRequest;
+import com.codeit.monew.domain.comment.dto.request.CommentUpdateRequest;
+import com.codeit.monew.domain.comment.dto.request.SortDirection;
 import com.codeit.monew.domain.comment.dto.response.CommentDto;
+import com.codeit.monew.domain.comment.dto.response.CommentPageResponse;
 import com.codeit.monew.domain.comment.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
@@ -18,9 +24,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
     private final CommentService commentService;
 
+    @GetMapping("api/comments")
+    public CommentPageResponse getComments(
+            @RequestParam UUID articleId,
+            @RequestParam(required = false) UUID userId,
+            @RequestParam CommentOrderBy orderBy,
+            @RequestParam SortDirection direction,
+            @RequestParam(required = false) String cursor,
+            @RequestParam(required = false) String after,
+            @RequestParam(defaultValue = "20") int limit
+    ) {
+        LocalDateTime afterDateTime =
+                after != null ? LocalDateTime.parse(after) : null;
+
+        return commentService.getComments(
+                articleId,
+                userId,
+                orderBy,
+                direction,
+                cursor,
+                LocalDateTime.parse(after),   // 여기서 타입 변환
+                limit
+        );
+    }
+
+
     @PostMapping
     public ResponseEntity<CommentDto> create(@Valid @RequestBody CommentRegisterRequest request) {
         CommentDto created = commentService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PatchMapping("api/comments/{commentId}")
+    public ResponseEntity<CommentDto> update(
+            @PathVariable UUID commentId,
+            @Valid @RequestBody CommentUpdateRequest request) {
+        CommentDto updated = commentService.update(commentId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    @DeleteMapping("api/comments/{commentId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID commentId) {
+        commentService.delete(commentId);
+        return ResponseEntity.noContent().build();
     }
 }
