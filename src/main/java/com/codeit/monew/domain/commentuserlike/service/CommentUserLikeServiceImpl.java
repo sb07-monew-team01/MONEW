3711ece2b1f8183efa1bdeb79f09ef7692113ecd
dogyeom <sprfv@naver.com>
2026-01-20 04:02:51 +0900

    fix: 커스텀 예외 적용(CommentAlreadyLikedException)

--- src/main/java/com/codeit/monew/domain/commentuserlike/service/CommentUserLikeServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/commentuserlike/service/CommentUserLikeServiceImpl.java
@@ -5,6 +5,7 @@ import com.codeit.monew.domain.comment.exception.CommentNotFoundException;
 import com.codeit.monew.domain.comment.repository.CommentRepository;
 import com.codeit.monew.domain.commentuserlike.dto.CommentUserLikeDto;
 import com.codeit.monew.domain.commentuserlike.entity.CommentUserLike;
+import com.codeit.monew.domain.commentuserlike.exception.CommentAlreadyLikedException;
 import com.codeit.monew.domain.commentuserlike.mapper.CommentUserLikeMapper;
 import com.codeit.monew.domain.commentuserlike.repository.CommentUserLikeRepository;
 import com.codeit.monew.domain.user.entity.User;
@@ -37,8 +38,9 @@ public class CommentUserLikeServiceImpl implements CommentUserLikeService {
                 commentUserLikeRepository.findByUserIdAndCommentId(userId, commentId);
 
         if (commentUserLikeRepository
-                .findByUserIdAndCommentId(userId, commentId).isPresent()) {
-            throw new IllegalStateException("이미 좋아요를 누른 댓글입니다.");
+                .findByUserIdAndCommentId(userId, commentId)
+                .isPresent()) {
+            throw new CommentAlreadyLikedException(ErrorCode.COMMENT_ALREADY_LIKED);
         }
 
         CommentUserLike like = CommentUserLike.create(user, comment);
