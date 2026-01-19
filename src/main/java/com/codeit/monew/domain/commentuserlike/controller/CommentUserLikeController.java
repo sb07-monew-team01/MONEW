0b1b0bfbc3cf835704807850aeb9a767066d7209
dogyeom <sprfv@naver.com>
2026-01-19 13:26:43 +0900

    feat: CommentUserLikeController 작성

---
+++ src/main/java/com/codeit/monew/domain/commentuserlike/controller/CommentUserLikeController.java
@@ -0,0 +1,26 @@
+package com.codeit.monew.domain.commentuserlike.controller;
+
+import com.codeit.monew.domain.commentuserlike.dto.CommentUserLikeDto;
+import com.codeit.monew.domain.commentuserlike.service.CommentUserLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comments/{commentId}/likes")
+public class CommentUserLikeController {
+    private final CommentUserLikeService commentUserLikeService;
+
+    @PostMapping
+    public ResponseEntity<CommentUserLikeDto> like(
+            @PathVariable UUID commentId,
+            @RequestParam UUID userId) {
+        CommentUserLikeDto response = commentUserLikeService.like(userId, commentId);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
