332c51455aac92a352c0bd91ade9628111a8d3cc
ChoiJiHye950 <principle950@daum.net>
2026-01-19 18:33:29 +0900

    fix : InterestRepository에 커스텀 인터페이스 추가
    - InterestRepositoryCustom을 추가하여 커스텀 기능 구현 준비
    - 관심사 관련 커스터마이징 구조 확장성을 위해 수정 적용

--- src/main/java/com/codeit/monew/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/codeit/monew/domain/interest/repository/InterestRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface InterestRepository extends JpaRepository<Interest, UUID> {
+public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestRepositoryCustom {
 }
