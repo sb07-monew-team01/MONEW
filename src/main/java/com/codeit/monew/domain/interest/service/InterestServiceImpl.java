a3461d729878689242803e47cd877c14174128de
ChoiJiHye950 <principle950@daum.net>
2026-01-19 21:08:05 +0900

    feat : 서비스 관심사 조회 기능 추가
    - getInterests 메서드를 InterestService 및 구현체에 추가
    - InterestRepositoryCustomImpl의 커스텀 조회 메서드 호출로 관심사 목록 반환
    - 테스트 코드에서 관심사 조회 로직 검증 추가

--- src/main/java/com/codeit/monew/domain/interest/service/InterestServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/interest/service/InterestServiceImpl.java
@@ -1,13 +1,19 @@
 package com.codeit.monew.domain.interest.service;
 
+import com.codeit.monew.domain.interest.dto.InterestCursorQuery;
 import com.codeit.monew.domain.interest.entity.Interest;
 import com.codeit.monew.domain.interest.exception.web.InterestNotFoundException;
 import com.codeit.monew.domain.interest.policy.InterestNamePolicy;
 import com.codeit.monew.domain.interest.repository.InterestRepository;
+import com.codeit.monew.domain.interest.repository.InterestRepositoryCustomImpl;
+import com.codeit.monew.domain.interest.vo.InterestOrderBy;
+import com.codeit.monew.domain.interest.vo.SortDirection;
 import com.codeit.monew.global.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +23,46 @@ import java.util.UUID;
 public class InterestServiceImpl implements InterestService{
     private final InterestRepository interestRepository;
     private final InterestNamePolicy interestNamePolicy;
+    private final InterestRepositoryCustomImpl interestRepositoryCustom;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Slice<Interest> getInterests(
+            String keyword,
+            String orderBy,
+            String direction,
+            String cursor,
+            String after,
+            Integer limit
+    ){
+        InterestOrderBy interestOrderBy = InterestOrderBy.valueOf(orderBy.toUpperCase());
+        SortDirection sortDirection = SortDirection.valueOf(direction);
+        String keywordValue = StringUtils.hasText(keyword) ? keyword.trim() : null;
+
+        String nameCursor = null;
+        Long subscriberCountCursor = null;
+
+        switch (interestOrderBy){
+            case NAME:
+                nameCursor = cursor;
+                break;
+            case SUBSCRIBER_COUNT:
+                subscriberCountCursor = Long.parseLong(cursor);
+                break;
+        }
+
+        InterestCursorQuery query = new InterestCursorQuery(
+            interestOrderBy,
+            sortDirection,
+            nameCursor,
+            subscriberCountCursor,
+            after,
+            limit,
+            keywordValue
+        );
+
+        return interestRepositoryCustom.findAllByCursor(query);
+    }
 
     @Override
     @Transactional
