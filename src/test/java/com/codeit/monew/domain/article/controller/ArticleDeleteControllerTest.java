cc2f116db7a2599ddf8a98e7960d94ab6da075f9
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-19 20:41:32 +0900

    refactor: Article 작업 내용 병합

--- src/test/java/com/codeit/monew/domain/article/controller/ArticleDeleteControllerTest.java
+++ src/test/java/com/codeit/monew/domain/article/controller/ArticleDeleteControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ArticleDeleteController.class)
+@WebMvcTest(ArticleController.class)
 @Import(GlobalExceptionHandler.class)
 class ArticleDeleteControllerTest {
     @Autowired
--- src/test/java/com/codeit/monew/domain/article/controller/ArticleSearchControllerTest.java
+++ src/test/java/com/codeit/monew/domain/article/controller/ArticleSearchControllerTest.java
@@ -15,8 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.SliceImpl;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ArticleSearchController.class)
+@WebMvcTest(ArticleController.class)
 class ArticleSearchControllerTest {
 
     private static final Logger log = LoggerFactory.getLogger(ArticleSearchControllerTest.class);
