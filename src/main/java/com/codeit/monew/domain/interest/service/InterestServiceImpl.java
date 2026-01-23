dc8e26f589b2eaee731690c291a7f2eefc11c3d6
ChoiJiHye950 <principle950@daum.net>
2026-01-23 17:04:32 +0900

    fix : 관심사 커서 페이지네이션 로직 수정 및 로그 추가
    - InterestCursorQuery의 파싱 로직 개선으로 코드 간소화
    - 커서 생성 로직에서 중복 및 불필요한 코드 제거
    - InterestServiceImpl에 @LogExecution 어노테이션과 디버깅용 로그 추가
    - 유지 보수성과 디버깅 용이성 향상 목표

--- src/main/java/com/codeit/monew/domain/interest/service/InterestServiceImpl.java
+++ src/main/java/com/codeit/monew/domain/interest/service/InterestServiceImpl.java
@@ -15,6 +15,8 @@ import com.codeit.monew.domain.interest.repository.InterestRepositoryCustomImpl;
 import com.codeit.monew.domain.interest.vo.InterestOrderBy;
 import com.codeit.monew.domain.interest.vo.NextCursor;
 import com.codeit.monew.domain.interestuser.repository.InterestUserRepository;
+import com.codeit.monew.global.aop.annotation.LogExecution;
+import com.codeit.monew.global.aop.annotation.LogTag;
 import com.codeit.monew.global.dto.PageResponse;
 import com.codeit.monew.global.enums.ErrorCode;
 import jakarta.persistence.EntityManager;
@@ -28,6 +30,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@LogExecution(tag= LogTag.INTEREST)
 public class InterestServiceImpl implements InterestService{
     private final InterestRepository interestRepository;
     private final InterestNamePolicy interestNamePolicy;
@@ -42,6 +45,7 @@ public class InterestServiceImpl implements InterestService{
     @Transactional(readOnly = true)
     public PageResponse<InterestCommonResponse> getInterests(UUID userId, InterestCursorPageRequest request) {
         InterestCursorQuery query = interestQueryMapper.toQuery(request);
+        System.out.println("2: ******************* "+ query.orderBy() + " - " + query.direction() + " : " + query.after() + "******************* ");
         Slice<Interest> slice = interestRepositoryCustom.findAllByCursor(query);
 
         List<InterestCommonResponse> content = slice.getContent().stream()
@@ -50,7 +54,7 @@ public class InterestServiceImpl implements InterestService{
                 interestUserRepository.existsByUserIdAndInterestId(userId, interest.getId())
             )
         ).toList();
-
+        System.out.println("3: ******************* "+ query.orderBy() + " - " + query.direction() + " : " + query.after() + "******************* ");
         NextCursor nextCursor = NextCursor.from(slice,
                 InterestOrderBy.fromString(request.orderBy()).orElse(InterestOrderBy.NAME));
 
