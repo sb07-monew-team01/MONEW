8d8adfd16861f5401e797f35ca86527639a04c0c
hyo37 <hyo37009@gmail.com>
2026-01-24 12:01:22 +0900

    feat: 사용자 활동 이벤트 리스너 구현
    - 사용자 활동 이벤트 처리 로직 추가
      - UserCreatedEvent, UserDeletedEvent: 사용자 생성 및 삭제 처리
      - CommentCreatedEvent, CommentLikeCreatedEvent: 댓글 및 댓글 좋아요 처리
      - ArticleViewedEvent: 아티클 조회수 처리
      - InterestUnsubscribedEvent: 관심사 구독 해제 처리
    - UserActivityService와 연동하여 사용자 활동 데이터를 관리하도록 구현

---
+++ src/main/java/com/codeit/monew/domain/userActivity/event/UserActivityEventListener.java
@@ -0,0 +1,53 @@
+package com.codeit.monew.domain.userActivity.event;
+
+import com.codeit.monew.domain.userActivity.service.UserActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserActivityEventListener {
+    private final UserActivityService userActivityService;
+
+    @EventListener
+    public void handleUserCreated(UserCreatedEvent event) {
+        userActivityService.createUserActivity(event.user());
+    }
+
+    @EventListener
+    public void handleUserDeleted(UserDeletedEvent event) {
+        userActivityService.remove(event.userId());
+    }
+
+    @EventListener
+    public void handleCommentCreated(CommentCreatedEvent event) {
+        userActivityService.addComment(
+                event.userId(),
+                event.comment(),
+                event.article(),
+                event.likeCount()
+        );
+    }
+
+    @EventListener
+    public void handleCommentLikeCreated(CommentLikeCreatedEvent event) {
+        userActivityService.addCommentLike(event.userId(), event.commentLike(), event.likeCount());
+    }
+
+    @EventListener
+    public void handleArticleViewed(ArticleViewedEvent event) {
+        userActivityService.addArticleView(
+                event.userId(),
+                event.articleView()
+        );
+    }
+
+    @EventListener
+    public void handleInterestUnsubscribed(InterestUnsubscribedEvent event) {
+        userActivityService.removeSubscription(
+                event.userId(),
+                event.interestId()
+        );
+    }
+}
