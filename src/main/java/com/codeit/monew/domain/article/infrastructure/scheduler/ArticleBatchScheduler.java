eeabf0c393ac27d7d9d70fc473a526ee6aa5b098
hyo37 <hyo37009@gmail.com>
2026-01-24 14:36:55 +0900

    feat: 글로벌 인증서 번들 파일(global-bundle.pem) 추가
    - SSL/TLS 통신에 필요한 인증서 번들 파일 추가
    - 인증서 체인으로 신뢰성 향상
    - 보안 통신을 위한 네트워크 구성 준비

--- src/main/java/com/codeit/monew/domain/article/infrastructure/scheduler/ArticleBatchScheduler.java
+++ src/main/java/com/codeit/monew/domain/article/infrastructure/scheduler/ArticleBatchScheduler.java
@@ -17,7 +17,7 @@ public class ArticleBatchScheduler {
     private final JobLauncher jobLauncher;
     private final Job articleCollectJob; // Config의 @Bean 메서드 명과 일치됨
 
-    @Scheduled(cron = "0 0/5 * * * *") // 5분마다 실행
+    @Scheduled(cron = "0 0 * * * *")
     public void runJob() {
         try {
             JobParameters params = new JobParametersBuilder()
