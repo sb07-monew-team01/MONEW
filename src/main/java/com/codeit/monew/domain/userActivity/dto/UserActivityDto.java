94fe0ce8e593360d0d7a5e5e93a66aec15d945a7
hyo37 <hyo37009@gmail.com>
2026-01-24 11:59:44 +0900

    feat: UserActivityDto 및 Mapper 구현
    - UserActivityDto 클래스를 record로 정의하여 사용자 활동 데이터 필드 추가
    - UserActivityDtoMapper에서 UserActivity 엔티티를 DTO로 변환하는 로직 구현

--- src/main/java/com/codeit/monew/domain/userActivity/dto/UserActivityDto.java
+++ src/main/java/com/codeit/monew/domain/userActivity/dto/UserActivityDto.java
@@ -1,4 +1,22 @@
 package com.codeit.monew.domain.userActivity.dto;
 
-public class UserActivityDto {
+import com.codeit.monew.domain.userActivity.entity.UserActivityArticleView;
+import com.codeit.monew.domain.userActivity.entity.UserActivityComment;
+import com.codeit.monew.domain.userActivity.entity.UserActivityCommentLike;
+import com.codeit.monew.domain.userActivity.entity.UserActivityInterestSubscription;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record UserActivityDto(
+        UUID userId,
+        String email,
+        String nickname,
+        LocalDateTime createdAt,
+        List<UserActivityInterestSubscription>subscriptions,
+        List<UserActivityComment> comments,
+        List<UserActivityCommentLike> commentLikes,
+        List<UserActivityArticleView> articleViews
+) {
 }
--- src/main/java/com/codeit/monew/domain/userActivity/mapper/UserActivityDtoMapper.java
+++ src/main/java/com/codeit/monew/domain/userActivity/mapper/UserActivityDtoMapper.java
@@ -6,7 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserActivityDtoMapper {
-    public UserActivityDto toDto(UserActivity dto) {
-        return null; // TODO
+    public UserActivityDto toDto(UserActivity entity) {
+        return new UserActivityDto(
+                entity.getUserId(),
+                entity.getEmail(),
+                entity.getNickname(),
+                entity.getCreatedAt(),
+                entity.getSubscriptions(),
+                entity.getComments(),
+                entity.getCommentLikes(),
+                entity.getArticleViews()
+        );
     }
 }
